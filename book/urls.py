c56e9c56b513ac19723974e1e07ff3dd29b4aa25
PhucToan-hue <phuctoan2709@gmail.com>
2026-06-22 14:31:19 +0700

    hoanthanh

--- book/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from book import views
-
-
-urlpatterns = [
-    path('books', views.books, name='books'),
-    path('books/<int:id>', views.book_detail, name='book-detail')
-]   
